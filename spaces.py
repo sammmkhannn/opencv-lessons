2e3ff5c5a7a1204b64b5e8b450f57d2a02087d0c
sam <shoaibur206@gmail.com>
2023-05-29 19:03:04 +0000

    add spaces.py

--- spaces.py
+++ spaces.py
@@ -1,1 +1,31 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
+
+img = cv.imread('images/FfNmgEUXgAIuV52 (1).jpeg')
+# cv.imshow('Home', img)
+
+# plt.imshow(img)
+# plt.show()
+# BGR to Grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# BGR to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow('HSV', hsv)
+
+# BGR to lab
+lab = cv.cvtColor(img, cv.COLOR_BGR2Lab)
+cv.imshow('LAB', lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow('RGB', rgb)
+
+
+# HSV to BGR
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow('HST2BGR', hsv_bgr)
+# plt.imshow(rgb)
+# plt.show()
+cv.waitKey(0)
